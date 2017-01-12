14b73db92c6516d693b9d0cedb8836f7eddda7c7
Tank4444 <Tank4444@mail.ru>
2017-01-13 00:58:51 +0700

    Hibernate DataBase working!!!!

---
+++ src/main/java/ru/chuikov/testweb/entity/TestClass.java
@@ -0,0 +1,59 @@
+package ru.chuikov.testweb.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Table(name = "testTable")
+public class TestClass {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment",strategy = "increment")
+    private long id;
+
+    @Column(name = "title",nullable = false,length = 50)
+    private String title;
+
+    @Column(name = "text",nullable = false)
+    private String text;
+
+    @Column(name = "data",nullable = false)
+    private Date date;
+
+    public TestClass() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
